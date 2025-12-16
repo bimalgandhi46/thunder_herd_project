72a1455f0049fbe01c85e832be0aa0adcd15e609
Bimal Gandhi <bimalgandhi46@gmail.com>
2025-12-16 09:29:50 -0600

    -parameter fix in pom

--- src/main/java/com/example/cache/controller/HealthController.java
+++ src/main/java/com/example/cache/controller/HealthController.java
@@ -3,12 +3,13 @@ package com.example.cache.controller;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 	@GetMapping("/health")
-    public Map<String, String> health() {
+    public Map<String, String> health(@RequestParam String nam) {
        return Map.of("status", "OK");
     }
 }
