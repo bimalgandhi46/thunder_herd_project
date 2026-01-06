4443d1daca956d73090816adf8ef20f3959313e8
Bimal Gandhi <bimalgandhi46@gmail.com>
2026-01-05 21:00:59 -0600

    Add redis caching:

--- src/main/java/com/example/cache/config/RedisConfig.java
+++ src/main/java/com/example/cache/config/RedisConfig.java
@@ -7,15 +7,21 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 @Configuration
 @EnableCaching
 public class RedisConfig {
 	@Bean
-	public RedisCacheConfiguration cacheConfiguration()
-	ObjectMapper mapper = new ObjectMapper(); 
-	mapper.registerModule(new JavaTimeModule()); 
-	mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-	{
-		return RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith( RedisSerializationContext.SerializationPair.fromSerializer(serializer) );
+	
+	public RedisCacheConfiguration cacheConfiguration() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(mapper);
+		return RedisCacheConfiguration.defaultCacheConfig()
+				.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
 	}
 }
