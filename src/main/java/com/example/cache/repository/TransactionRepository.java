d9aca8ac3c0054c9aea782557d413a20bf6c1bf0
Bimal Gandhi <bimalgandhi46@gmail.com>
2026-01-13 14:32:24 -0600

    Add redis caching:

--- src/main/java/com/example/cache/repository/TransactionRepository.java
+++ src/main/java/com/example/cache/repository/TransactionRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.example.cache.entity.Transactions;
 
-import java.util.List;
+
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
