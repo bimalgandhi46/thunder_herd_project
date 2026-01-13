c7a828836b3c63b699184939779ebbe2dcf70cc0
Bimal Gandhi <bimalgandhi46@gmail.com>
2026-01-13 09:18:31 -0600

    Add redis caching:

---
+++ src/main/java/com/example/cache/config/SentinelRedisConnectionConfig.java
@@ -0,0 +1,23 @@
+package com.example.cache.config;
+
+import java.time.Duration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+import io.lettuce.core.ReadFrom;
+
+@Configuration
+public class SentinelRedisConnectionConfig {
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration().master("mymaster")
+				.sentinel("10.0.0.2", 26379).sentinel("10.0.0.3", 26379).sentinel("10.0.0.4", 26379);
+		LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+				.readFrom(ReadFrom.REPLICA_PREFERRED).commandTimeout(Duration.ofSeconds(3)).build();
+		return new LettuceConnectionFactory(sentinelConfig, clientConfig);
+	}
+}
