691efd4e9e774bae59d8263a5a163a7e7409bb55
Bimal Gandhi <bimalgandhi46@gmail.com>
2026-01-13 14:28:18 -0600

    Add redis caching:

--- src/main/java/com/example/cache/repository/TransactionRepository.java
+++ src/main/java/com/example/cache/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package com.example.cache.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.cache.entity.Transactions;
 
@@ -8,7 +9,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
+@Repository
 public interface TransactionRepository extends JpaRepository<Transactions, Long>{
 	Page<Transactions> findByDomainIgnoreCase(String domain, Pageable pageable);
     Page<Transactions> findByLocationIgnoreCase(String location, Pageable pageable);
