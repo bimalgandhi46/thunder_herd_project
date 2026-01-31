4155875431d158315e29c76a62db81c9e30cb3bd
Bimal Gandhi <bimalgandhi46@gmail.com>
2026-01-31 14:22:18 -0600

    increase thread pool to 300

--- src/main/java/com/example/cache/service/ListCacheService.java
+++ src/main/java/com/example/cache/service/ListCacheService.java
@@ -1,5 +1,6 @@
 package com.example.cache.service;
 
+import com.example.cache.dto.TransactionDto;
 import com.example.cache.entity.Transactions;
 import com.example.cache.repository.TransactionRepository;
 
@@ -12,20 +13,39 @@ import java.util.List;
 @Service
 public class ListCacheService {
 
-    private final TransactionRepository repository;
-    private final CacheLockManager lockManager;
+	private final TransactionRepository repository;
+	private final CacheLockManager lockManager;
 
-    public ListCacheService(TransactionRepository repository, CacheLockManager lockManager) {
-        this.repository = repository;
-        this.lockManager = lockManager;
-    }
+	public ListCacheService(TransactionRepository repository, CacheLockManager lockManager) {
+	    this.repository = repository;
+	    this.lockManager = lockManager;
+	}
 
-    @Cacheable(
-            value = "transactionsByDomain",
-            key = "'v2:' + #domain + ':' + #pageable.pageNumber + ':' + #pageable.pageSize"  )
-    public List<Transactions> getByDomainCached(String domain, Pageable pageable) 
-    { 
-    	return repository .findByDomainIgnoreCase(domain, pageable) .getContent();
-    }
-    }
+	private TransactionDto toDto(Transactions t) {
+		return new TransactionDto(t.getId(), 
+				t.getDate(), t.getDomain(), 
+				t.getLocation(), 
+				t.getValue(),
+				t.getTransactionCount());
+	}
 
+	@Cacheable(
+	        value = "transactionsByDomain",
+	        key = "'v3:' + #domain + ':' + #pageable.pageNumber + ':' + #pageable.pageSize"
+	)
+	public List<TransactionDto> getByDomainCached(String domain, Pageable pageable) {
+
+	    Object lock = lockManager.getLock(domain);
+
+	    synchronized (lock) {
+	        System.out.println("Synchronized ListCacheService");
+
+	        return repository
+	                .findByDomainIgnoreCase(domain, pageable)
+	                .getContent()
+	                .stream()
+	                .map(this::toDto)
+	                .toList();
+	    }
+}
+}
