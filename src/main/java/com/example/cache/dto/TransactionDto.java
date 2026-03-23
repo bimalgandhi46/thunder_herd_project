3109ebafd00c05b8c50d0b399437df2e49677cbb
Bimal Gandhi <bimalgandhi46@gmail.com>
2026-03-23 12:06:06 -0500

    validation

--- src/main/java/com/example/cache/dto/TransactionDto.java
+++ src/main/java/com/example/cache/dto/TransactionDto.java
@@ -1,13 +1,15 @@
 package com.example.cache.dto;
 
 import java.time.LocalDate;
-
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
 public record TransactionDto(
 		Long id,
-        LocalDate date,
-        String domain,
-        String location,
-        Long value,
-        Integer transactionCount
+        @NotNull LocalDate date,
+        @NotBlank String domain,
+        @NotBlank String location,
+        @NotNull @Min(0) Long value,
+        @NotNull @Min(0) Integer transactionCount
 		
 		) {}
